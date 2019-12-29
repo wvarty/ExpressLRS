a7bca21a2faf0993a8c952a0121db271e436200c
Jye <JyeSmith@users.noreply.github.com>
2019-12-29 13:28:05 +1000

    r9m target pinout

--- src/src/targets.h
+++ src/src/targets.h
@@ -84,32 +84,29 @@
 #endif
 
 #ifdef TARGET_R9M_RX
-#define GPIO_PIN_NSS -1 // not sure if for thesis so no ascii dwg this time - Chris
-#define GPIO_PIN_DIO0 -1
-#define GPIO_PIN_DIO1 -1
-#define GPIO_PIN_MOSI PB15
-#define GPIO_PIN_MISO -1
-#define GPIO_PIN_SCK -1
-#define GPIO_PIN_RST -1
+#define GPIO_PIN_NSS         PB12
+#define GPIO_PIN_DIO0        PA0
+#define GPIO_PIN_DIO1        PA1 // NOT CORRECT!!! PIN STILL NEEDS TO BE FOUND BUT IS CURRENTLY UNUSED
+#define GPIO_PIN_MOSI        PB15
+#define GPIO_PIN_MISO        PB14
+#define GPIO_PIN_SCK         PB13
+#define GPIO_PIN_RST         PC14
 #define GPIO_PIN_RX_ENABLE -1
 #define GPIO_PIN_TX_ENABLE -1
 #define GPIO_PIN_OLED_SDA -1
 #define GPIO_PIN_OLED_SCK -1
-#define GPIO_PIN_RCSIGNAL_RX PA10 //not relevant, can use only default for esp8266=esp8285
+#define GPIO_PIN_RCSIGNAL_RX PA10
 #define GPIO_PIN_RCSIGNAL_TX PA9
-#define GPIO_PIN_LED C1
-#endif
+#define GPIO_PIN_LED         PC1 // Red
+#define GPIO_PIN_LED_GEEN    PB3 // Green - Currently unused
 
-// #sbus 1/3 A2
-// #sport/fport 1/6 AND 2/11 A5 and B10
-// #inverted sport 2/12 B11
-// #ch4 1/11 A10
-// #ch3 1/10 A9
-// #ch2 1/12 A11
-// #ch1 1/9 A8
-// #red led C1
-// #nrst 3/15 C14
-// #chip select 2/13 B12
-// #MOSI 2/16 B15
-// #MISO 2/13 AND 2/15 B12 and B14
-// #CK 2/14 B13
+// External pads
+// #define R9m_Ch1    PA8
+// #define R9m_Ch2    PA11
+// #define R9m_Ch3    PA9
+// #define R9m_Ch4    PA10
+// #define R9m_sbus   PA2
+// #define R9m_sport  PA5
+// #define R9m_isport PB11
+
+#endif
